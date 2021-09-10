017b77ebea59cf29620f60000144a86250323b8b
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-09-10 20:36:16 +0300

    pushing articles view function and articles html page

--- app/views.py
+++ app/views.py
@@ -8,5 +8,13 @@ def index():
     View root page function that returns the index page and it's content
     """
     title  = "News Centre. "
-    heading = "Welcome to News Centre, A site for all the news from various sources"
+    heading = "Welcome to News Centre, A site for all the news from various sources. \nThis page will have all the news outlets/sources"
     return render_template('index.html', title=title, heading=heading)
+
+@app.route('/articles/<int:source_id>')
+def articles(source_id):
+    """
+    View page function that returns articles page and and its content.
+    """
+    articles_title = "This page will have all the articles from selected sources"
+    return render_template("articles.html",id = source_id, articles_title = articles_title)
