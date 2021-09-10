9a28e2b80f23c7cafc61817aa1faebae51776736
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-09-10 20:47:10 +0300

    pushing configuration classes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from app.config import DevConfig
 
 #Initializing the application
 app = Flask(__name__)
+
+#Setting up configurations
+app.config.from_object(DevConfig)
 
 from app import views
